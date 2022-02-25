40f39198034d8c9188fafde7074c121ac51de20e
shlee <h983019@naver.com>
2022-02-26 01:38:35 +0900

    Day2 Wordcountsort

--- src/Driver.java
+++ src/Driver.java
@@ -10,6 +10,7 @@ public class Driver {
 
 			pgd.addClass("wordcount", Wordcount.class, "A map/reduce program that performs word counting.");
 			pgd.addClass("wordcount1char", Wordcount1char.class, "A map/reduce program that that counts the 1st character of words in the input files.");
+			pgd.addClass("wordcountsort", Wordcountsort.class, "A map/reduce program that output frequency of the words in the input files by alphabetical order.");
 
       			pgd.driver(args);
 			exitCode = 0;
